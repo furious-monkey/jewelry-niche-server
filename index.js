ca4a36132fd7c2b6ffc3ee07ecd94a416c5b39dd
abu said <mdabusaidriyaz@gmail.com>
2021-11-10 00:55:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ha2x2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("jewelry_store");
+        const jewelry = database.collection("jewelry");
+
+        app.get("/jewelry/", async (req, res) => {
+            const cursor = await jewelry.find({}).toArray();
+            res.send(cursor);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+// pass : yiCEzimhGU8CnaQK
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, (req, res) => {
+    console.log("Running with port: " + port);
+})
